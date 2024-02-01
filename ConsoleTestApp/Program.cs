93596d160e7ac94cee7962a23a6a257028b63499
Lucas Juan <ljgsodre@outlook.com>
2024-02-01 18:44:52 -0300

    Call  example

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -3,19 +3,12 @@
 EmailDispatcher dispatcher = new EmailDispatcher();
 
 dynamic spamEmail = new { type = "spam" };
-dynamic fanEmail = new { type = "fan" };
-dynamic complaintEmail = new { type = "complaint" };
-dynamic miscEmail = new { type = "misc" };
-dynamic unknownEmail = new { type = "unknown" };
 
 dispatcher.Dispatch(spamEmail);
-dispatcher.Dispatch(fanEmail);
-dispatcher.Dispatch(complaintEmail);
-dispatcher.Dispatch(miscEmail);
 
 try
 {
-    dispatcher.Dispatch(unknownEmail);
+    dispatcher.Dispatch(spamEmail);
 }
 catch (ArgumentException e)
 {
